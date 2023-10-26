f7f8cd9968686194b873cd8228b0f9332c41bc55
Limbo <komapo4ek@gmail.com>
2023-10-27 00:38:51 +0200

    sersion 0.0.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,36 @@
 "use strict"
-
+import * as Swiper from "./slider.js";
 import * as flsFunction from "./modules/functions.js";
 
-// flsFunction.isWebp();
+    // flsFunction.isWebp();
+    
+    // import Swiper, { Navigation, Pagination } from 'swiper';
+    
+    // const swiper = new Swiper('.swiper', {});;
+    
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+document.addEventListener('DOMContentLoaded',()=> {
 
-// const swiper = new Swiper('.swiper', {});
+    const btnCatalog = document.querySelector('.header__select');
+    const catalogMenu = document.querySelector('.sidebar__list');
+
+    const openHeadMenu = () => {
+        if(!btnCatalog.parentElement.querySelector("svg").classList.contains('arrow-down')){  
+            btnCatalog.parentElement.querySelector("svg").classList.add('arrow-down');
+            btnCatalog.parentElement.querySelector("svg").classList.remove('arrow-up');
+            catalogMenu.classList.add('sidebar__list--active');
+        }
+        else{
+            btnCatalog.parentElement.querySelector("svg").classList.remove('arrow-down');
+            btnCatalog.parentElement.querySelector("svg").classList.add('arrow-up');
+            catalogMenu.classList.remove('sidebar__list--active');
+        }
+    }
+    btnCatalog.addEventListener('click', openHeadMenu)
+
+
+
+})
+
+
+// btnCatalog.parentElement.querySelector("svg").style.cssText='transform: rotate(0deg)';
